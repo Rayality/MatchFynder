80aa6672d96333bcf48f1dcb9331cb7025933230
Nicholas Tomasso <tomassonicholas@gmail.com>
2023-06-28 17:30:58 -0400

    Adds JWT functionality and account creation

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -2,4 +2,37 @@ from pydantic import BaseModel
 
 
 class AccountOut(BaseModel):
+    id: str
+    first_name: str
+    last_name: str
+    email: str
+    username: str
+
+
+class HashedAccountOut(AccountOut):
+    hashed_password: str
+
+
+class AccountIn(BaseModel):
+    username: str
+    email: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    hashed_password: str
+    disabled: bool | None = None
+
+
+class AccountRepo(BaseModel):
+    def get(self, email: str) -> HashedAccountOut:
+        return ()
+
+    def create(
+        self,
+        info: AccountIn,
+        hashed_password: str,
+    ) -> HashedAccountOut:
+        return ()
+
+
+class DuplicateAccountError(BaseModel):
     pass
