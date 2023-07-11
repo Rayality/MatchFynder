4f62172e3050a94eb21413979ad50c23128bf229
Ray <charlesraycowan@yahoo.com>
2023-07-11 08:45:08 -0400

    created api endpoints and functions for google query

--- api/queries/generic_sql.py
+++ api/queries/generic_sql.py
@@ -1,5 +1,6 @@
 from .pool import pool
 from psycopg import sql
+from psycopg.rows import dict_row
 
 
 def generic_insert(table_name, data):
@@ -21,4 +22,20 @@ def generic_insert(table_name, data):
                 data['id'] = id
                 return (data)
     except Exception as e:
-        return e
+        return {"message": f"{e}"}
+
+
+def generic_find(table_name: str, where_identifier: str, where_equal_to: str):
+    query = sql.SQL("SELECT * FROM {t} WHERE {i} = {e}").format(
+        t=sql.Identifier(table_name),
+        i=sql.Identifier(where_identifier),
+        e=where_equal_to
+    )
+    try:
+        with pool.connection() as conn:
+            with conn.cursor(row_factory=dict_row) as db:
+                result = db.execute(query)
+                data = result.fetchone()
+                return (data)
+    except Exception as e:
+        return {"message": f"{e}"}
