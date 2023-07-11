09575780be4db5f4dfb8f580b1e0cb630b60614c
Nicholas Tomasso <tomassonicholas@gmail.com>
2023-07-11 17:39:53 -0400

    refactored Login and Create Account into modal component on Nav Bar

--- ghi/src/Components/Nav.js
+++ ghi/src/Components/Nav.js
@@ -1,64 +1,84 @@
 import { NavLink } from "react-router-dom";
+import ModalForm from "./forms/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { shown } from "../Redux/modal-slice";
 
 function Nav() {
+  const dispatch = useDispatch();
+  const initModal = () => {
+    dispatch(shown());
+  };
+
   return (
-    <nav className='navbar bg-body-tertiary'>
-      <div className='container-fluid'>
-        {/* <NavLink className="navbar-brand" href="#">Finder</NavLink> */}
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='offcanvas'
-          data-bs-target='#offcanvasNavbar'
-          aria-controls='offcanvasNavbar'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div
-          className='offcanvas offcanvas-end'
-          tabIndex='-1'
-          data-bs-toggle='true'
-          id='offcanvasNavbar'
-          aria-labelledby='offcanvasNavbarLabel'
-        >
-          <div className='offcanvas-header'>
-            <h5 className='offcanvas-title' id='offcanvasNavbarLabel'>
-              Offcanvas
-            </h5>
-            <button
-              type='button'
-              className='btn-close'
-              data-bs-dismiss='offcanvas'
-              aria-label='Close'
-            ></button>
-          </div>
-          <div className='offcanvas-body'>
-            <ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
-              <li className='nav-item'>
-                {/* <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink> */}
-              </li>
-              <li className='nav-item'>
-                <NavLink className='nav-link' to='/logout'>
-                  Logout
-                </NavLink>
-              </li>
-            </ul>
-            <form className='d-flex mt-3' role='search'>
-              <input
-                className='form-control me-2'
-                type='search'
-                placeholder='Search'
-                aria-label='Search'
-              />
-              <button className='btn btn-outline-success' type='submit'>
-                Search
-              </button>
-            </form>
+    <>
+      <nav className='navbar bg-body-tertiary'>
+        <div className='container-fluid'>
+          {/* <NavLink className="navbar-brand" href="#">Finder</NavLink> */}
+          <button
+            onClick={initModal}
+            className='btn btn-primary'
+            data-bs-toggle='modal'
+            data-bs-target='#loginModal'
+          >
+            Insert Profile person thumbnail here!
+            {/* replace this with a vector image, ask Nick if this doesnt make sense */}
+          </button>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='offcanvas'
+            data-bs-target='#offcanvasNavbar'
+            aria-controls='offcanvasNavbar'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div
+            className='offcanvas offcanvas-end'
+            tabIndex='-1'
+            data-bs-toggle='true'
+            id='offcanvasNavbar'
+            aria-labelledby='offcanvasNavbarLabel'
+          >
+            <div className='offcanvas-header'>
+              <h5 className='offcanvas-title' id='offcanvasNavbarLabel'>
+                Offcanvas
+              </h5>
+              <button
+                type='button'
+                className='btn-close'
+                data-bs-dismiss='offcanvas'
+                aria-label='Close'
+              ></button>
+            </div>
+            <div className='offcanvas-body'>
+              <ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
+                <li className='nav-item'>
+                  {/* <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink> */}
+                </li>
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='/logout'>
+                    Logout
+                  </NavLink>
+                </li>
+              </ul>
+              <form className='d-flex mt-3' role='search'>
+                <input
+                  className='form-control me-2'
+                  type='search'
+                  placeholder='Search'
+                  aria-label='Search'
+                />
+                <button className='btn btn-outline-success' type='submit'>
+                  Search
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <ModalForm id='loginModal' />
+    </>
   );
 }
 export default Nav;
--- ghi/src/Redux/store.js
+++ ghi/src/Redux/store.js
@@ -4,6 +4,7 @@ import loginReducer from "./login-slice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { optionsApi } from "./optionsApi";
 import { searchApi } from "./searchApi";
+import modalReducer from "./modal-slice";
 
 export const store = configureStore({
   reducer: {
@@ -11,6 +12,7 @@ export const store = configureStore({
     accountLogin: loginReducer,
     [optionsApi.reducerPath]: optionsApi.reducer,
     [searchApi.reducerPath]: searchApi.reducer,
+    modalCheck: modalReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(optionsApi.middleware, searchApi.middleware),
