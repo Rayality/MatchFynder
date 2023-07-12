e16b86ee60013622e60454451326f2642b90175a
Jarrett Pitts <JarrettPitts@yahoo.com>
2023-07-12 12:27:46 -0400

    Backend logic to check match_made is true

--- api/routers/search.py
+++ api/routers/search.py
@@ -6,8 +6,7 @@ from queries.search import (
     SearchRepository,
     SearchOptionsLink,
     SearchDad,
-    MatchMade
-
+    MatchMade,
 )
 from queries.options import Error
 from typing import Union
@@ -16,55 +15,68 @@ router = APIRouter()
 
 
 @router.post(
-        "/search/{search_id}/options/{option_id}", response_model=Union[Error, int]
+    "/search/{search_id}/options/{option_id}", response_model=Union[Error, int]
 )
-def update_edible_option(s: SearchOptionsLink, response: Response, repo: SearchRepository = Depends()):
+def update_edible_option(
+    s: SearchOptionsLink,
+    response: Response,
+    repo: SearchRepository = Depends(),
+):
     return repo.update_edible_count(s.search_id, s.option_id)
 
 
 @router.get(
-        "/search/{search_id}/options", response_model=Union[list[SearchOptions], Error]
+    "/search/{search_id}/options",
+    response_model=Union[list[SearchOptions], Error],
 )
-def get_search_options(search_id: int, response: Response, repo: SearchRepository = Depends()):
+def get_search_options(
+    search_id: int, response: Response, repo: SearchRepository = Depends()
+):
     return repo.get_search_options(search_id)
 
+
 #
-@router.post(
-    "/search/{search_id}/options"
-)
-def add_search_option(s: SearchOptionsLink, response: Response, repo: SearchRepository = Depends()):
+@router.post("/search/{search_id}/options")
+def add_search_option(
+    s: SearchOptionsLink,
+    response: Response,
+    repo: SearchRepository = Depends(),
+):
     return repo.add_search_option(s.search_id, s.option_id)
 
+
 @router.get(
-        "/search/{search_id}/finders", response_model=Union[list[int], Error]
+    "/search/{search_id}/finders", response_model=Union[list[int], Error]
 )
-def get_search_finders(search_id: int, response: Response, repo: SearchRepository = Depends()):
+def get_search_finders(
+    search_id: int, response: Response, repo: SearchRepository = Depends()
+):
     return repo.get_search_finders(search_id)
 
 
-@router.post(
-        "/search/create", response_model=Union[Error, SearchOut]
-)
-def create_search(search: Search, response: Response, repo: SearchRepository = Depends()):
+@router.post("/search/create", response_model=Union[Error, SearchOut])
+def create_search(
+    search: Search, response: Response, repo: SearchRepository = Depends()
+):
     return repo.create_search(search)
 
 
-
-
-@router.get(
-    "/search/", response_model=Union[list[SearchDad], Error]
-)
+@router.get("/search/", response_model=Union[list[SearchDad], Error])
 def get_searches(response: Response, repo: SearchRepository = Depends()):
     return repo.get_search()
 
-@router.get(
-    "/search/{search_id}/", response_model = Union[Search, Error]
-)
-def get_single_search(search_id: int, response: Response, repo: SearchRepository = Depends()) -> Search:
+
+@router.get("/search/{search_id}/", response_model=Union[SearchDad, Error])
+def get_single_search(
+    search_id: int, response: Response, repo: SearchRepository = Depends()
+) -> SearchDad:
     return repo.get_single_search(search_id)
 
+
 @router.get(
-    "/search/{search_id}/match_made", response_model =Union[MatchMade, Error]
+    "/search/{search_id}/match_made", response_model=Union[MatchMade, Error]
 )
-def get_match_made(search_id: int, response: Response, repo: SearchRepository = Depends()) -> MatchMade:
+def get_match_made(
+    search_id: int, response: Response, repo: SearchRepository = Depends()
+) -> MatchMade:
     return repo.get_match_made(search_id)
