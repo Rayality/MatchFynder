6e2cf1afe8c187e2c777527afe02f3d25a0f6d7f
Ray <charlesraycowan@yahoo.com>
2023-07-27 17:34:05 -0400

    removed os import

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from auth.authenticator import authenticator
-import os
 from routers import options, search, accounts, places_api
 from socks import search_socket
 
