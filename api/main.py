3d61ec384f2d373be9dc6572818ec3c19adbebfa
Ray <charlesraycowan@yahoo.com>
2023-07-27 17:31:48 -0400

    cors changed

--- api/main.py
+++ api/main.py
@@ -15,12 +15,16 @@ app.include_router(accounts.router)
 app.include_router(search.router)
 app.include_router(search_socket.router)
 # app.include_router(send_mail.router)
-
 app.include_router(places_api.router)
+
+origins = [
+    "http://localhost:3000",
+    "https://incognitoincredibles.gitlab.io/module3-project-finder/"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", os.environ.get("CORS_HOST", None)],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
