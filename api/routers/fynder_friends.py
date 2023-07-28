4029682b38e5b2a12e6580677f4eaf5781b914f1
Ray <charlesraycowan@yahoo.com>
2023-07-28 19:53:46 -0400

    endpoints for friends

--- api/routers/fynder_friends.py
+++ api/routers/fynder_friends.py
@@ -7,6 +7,25 @@ from queries.options import (
     Error,
 )
 from typing import Union
+from queries.fynder_friends import FynderRepository
 
 
 router = APIRouter
+
+
+@router.post("/friend/{owner}/add/")
+def add_friend(
+    owner: int,
+    friend_id: int,
+    response: Response,
+    repo: FynderRepository=Depends()
+):
+    return repo.add_friend(owner, friend_id)
+
+
+@router.get("/friend/{owner}")
+def get_friends(
+    owner: int,
+    repo: FynderRepository=Depends()
+):
+    return repo.get_friends(owner)
