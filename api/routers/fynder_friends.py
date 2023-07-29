2ec502c4762db94669bde615f4ee541a71210b4e
Nicholas Tomasso <tomassonicholas@gmail.com>
2023-07-29 18:42:34 -0400

    allows for people in a search to be added to the search_fynders table if they are not already present

--- api/routers/fynder_friends.py
+++ api/routers/fynder_friends.py
@@ -1,12 +1,33 @@
 from fastapi import APIRouter, Depends, Response, HTTPException, status
 from auth.authenticator import authenticator
-from queries.options import (
-    OptionIn,
-    OptionOut,
-    OptionRepository,
-    Error,
+from queries.search import (
+    SearchRepository,
 )
 from typing import Union
 
 
 router = APIRouter
+not_authorized = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid authentication credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+@router.post("search/{search_id}/include")
+def include_in_search(
+    search_id: int,
+    repo: SearchRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    if account_data:
+        account_id = account_data.get("id")
+        try:
+            # adds this user to this search participant table
+            repo.add_search_finder(account_id, search_id)
+
+        except Exception:
+            pass
+
+    else:
+        raise not_authorized
