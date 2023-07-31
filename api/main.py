00893389b6b899bf1a5a7daf8da9eba9824d6650
Nicholas Tomasso <tomassonicholas@gmail.com>
2023-07-31 00:39:25 +0000

    are we even looking in the right spot?

--- api/main.py
+++ api/main.py
@@ -17,15 +17,15 @@ app.include_router(search_socket.router)
 # app.include_router(send_mail.router)
 app.include_router(places_api.router)
 
-origins = [
-    "http://localhost:3000",
-    # os.environ.get("CORS_HOST", None),
-    "https://incognitoincredibles.gitlab.io",
-]
+# origins = [
+#     "http://localhost:3000",
+#     os.environ.get("CORS_HOST", None),
+#     "https://incognitoincredibles.gitlab.io",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
